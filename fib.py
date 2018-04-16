88f4714d3051e0b268263e0197bc4888bd110750
Elyssa Hofgard <elyssahofgard@DN0a1f4345.SUNet>
2018-04-15 20:10:05 -0700

    Lab 4.

--- fib.py
+++ fib.py
@@ -29,7 +29,9 @@ Usage
     elif args[0] == "approx" and len(args) == 2:
         phi_approx(int(args[1]))
     elif args[0] == "converge" and len(args) == 1:
-        phi_converge()
+        phi_converge_print()
+    elif args[0] == "converge" and len(args) == 2:
+        phi_converge(str(args[1]))
     else:
         print "Error: input not understood.\n" \
                 "    Type './fib.py help' for info on this program."
@@ -67,7 +69,7 @@ phi_converge_output_format = \
     phi_old: {:.25f}
     phi_new: {:.25f}"""
 
-def phi_converge():
+def phi_converge_print():
     """Keep calculating higher-order Fibonacci approximations to the golden
     ratio until it stops changing (to floating-point precision)."""
 
@@ -81,4 +83,19 @@ def phi_converge():
         print phi_converge_output_format.format(i, phi_new, phi_old)
     print "\nConverged to %.25f" % phi_new
 
+def phi_converge(filename):
+# creates the file so the user does not have to
+    file = open(filename,"w+")
+    i = 3
+    phi_old = phi_approx(i-1, show_output=False)
+    phi_new = phi_approx(i)
+    while phi_old != phi_new:
+        i += 1
+        phi_old = phi_new
+        phi_new = phi_approx(i, show_output=False)
+        output = phi_converge_output_format.format(i, phi_new, phi_old)
+        file.write(output)
+    file.write("\nConverged to %.25f" % phi_new)
+    file.close()
+    
 if __name__ == '__main__': main()
